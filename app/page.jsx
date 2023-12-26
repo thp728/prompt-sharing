0def0860bdbd801c88fbd601fc5ee3e04e0dcce9
thp728 <thp728@gmail.com>
2023-12-26 12:12:17 +0530

    Completed Create Prompt Page

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover & Share
+        Discover & Share {` `}
         <br className="max-md:hidden" />
         <span className="orange_gradient text-center">AI Powered Prompts</span>
       </h1>
